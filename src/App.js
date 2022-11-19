fd06a143fdb91fe99c1ad4af9f7a576f7c468c2a
Kush <85061303+kush992@users.noreply.github.com>
2022-11-19 11:48:42 +0100

    changes: css & search module

--- src/App.js
+++ src/App.js
@@ -8,8 +8,24 @@ const App = () => {
   const [query, setQuery] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
+//   const addEntry = () => {
+//     // Parse any JSON previously stored in allEntries
+//     let existingEntries = JSON.parse(localStorage.getItem("allEntries"));
+//     if(existingEntries == null) existingEntries = [];
+//     const entryTitle = document.getElementById("entryTitle").value;
+//     const entryText = document.getElementById("entryText").value;
+//     const entry = {
+//         "title": entryTitle,
+//         "text": entryText
+//     };
+//     localStorage.setItem("entry", JSON.stringify(entry));
+//     // Save allEntries back to local storage
+//     existingEntries.push(entry);
+//     localStorage.setItem("allEntries", JSON.stringify(existingEntries));
+// };
+
   const search = async (event) => {
-    if (query && query.length > 3 && event.key === "Enter") {
+    // if (query && query.length > 3 && event.key === "Enter") {
       try {
         const data = await fetchweather(query);
         setWeather(data);
@@ -19,8 +35,25 @@ const App = () => {
         console.error(error);
         alert(error);
       }
-    }
+    // }
+
+    let existingQuery = JSON.parse(localStorage.getItem('allItems'));
+    if(existingQuery === null) existingQuery = [];
+    const history = {
+      "cityName" : query
+    };
+    localStorage.setItem('history', JSON.stringify(history));
+
+    const array = localStorage.getItem('allItems');
+    const hist = (localStorage.getItem('history'));
+    console.log(existingQuery.length)
+    // if(!array?.includes(hist) && existingQuery.length < 10) {
+      existingQuery.push(history);
+    // }
+    localStorage.setItem('allItems', JSON.stringify(existingQuery));
   };
+
+  const ar = JSON.parse(localStorage.getItem('allItems'))
 
   useEffect(() => {
     if (query && query.length < 4) {
@@ -37,9 +70,15 @@ const App = () => {
             placeholder="city"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
-            onKeyPress={search}
+            // onKeyPress={search}
           />
+          <button onClick={search}>Search</button>
         </div>
+        {ar?.slice(-10).reverse().map((item) => {
+          return (
+            <div onClick={()=>setQuery(item.cityName)}>{item.cityName}</div>
+          )
+        })}
         {errorMessage && <p className="errorMessage">{errorMessage}</p>}
       </div>
       {weather && <WeatherCard weather={weather} />}
